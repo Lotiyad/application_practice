48721e8df7cd0e1ab443f61b9ad37c2e14e20e82
Loti Yadeta <loti50435@gmail.com>
2024-12-22 22:31:27 +0300

    newfile

---
+++ demo/src/main/java/org/springframework/context/support/ClassPathXmlApplicationContext.java
@@ -0,0 +1,22 @@
+package org.springframework.context.support;
+
+import com.itsc.ioc.springioc.student;
+
+public class ClassPathXmlApplicationContext {
+
+	public student getBean(String string, Class<student> class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public student bean2(String string, Class<student> class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public student getBean1(String string, Class<student> class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
